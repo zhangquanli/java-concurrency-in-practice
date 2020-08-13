86fc2854c63115576e2f66b56d1ea97c5f9c3327
zhangquanli <502624709@qq.com>
2020-08-13 08:59:26 +0800

    5.6 构建高效且可伸缩的结果缓存

---
+++ src/main/java/chapter_05/code_16/ExpensiveFunction.java
@@ -0,0 +1,11 @@
+package chapter_05.code_16;
+
+import java.math.BigInteger;
+
+public class ExpensiveFunction implements Computable<String, BigInteger> {
+
+    public BigInteger compute(String arg) {
+        // after deep thought...
+        return new BigInteger(arg);
+    }
+}
---
+++ src/main/java/chapter_05/code_16/Memoizer1.java
@@ -0,0 +1,30 @@
+package chapter_05.code_16;
+
+import net.jcip.annotations.GuardedBy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Initial cache attempt using HashMap and synchronization.
+ */
+public class Memoizer1<A, V> implements Computable<A, V> {
+    @GuardedBy("this")
+    private final Map<A, V> cache = new HashMap<>();
+    private final Computable<A, V> c;
+
+    public Memoizer1(Computable<A, V> c) {
+        this.c = c;
+    }
+
+    public synchronized V compute(A arg) throws InterruptedException {
+        V result = cache.get(arg);
+        if (result == null) {
+            result = c.compute(arg);
+            cache.put(arg, result);
+        }
+        return result;
+    }
+}
+
+
