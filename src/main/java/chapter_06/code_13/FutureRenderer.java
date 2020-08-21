768e34db36f0513c4cec86a054bdeef6b78f1a85
zhangquanli <502624709@qq.com>
2020-08-21 08:50:01 +0800

    6.3.3 示例：使用Future实现页面渲染器

--- src/main/java/chapter_06/code_13/FutureRenderer.java
+++ src/main/java/chapter_06/code_13/FutureRenderer.java
@@ -1,4 +1,53 @@
 package chapter_06.code_13;
 
-public class FutureRenderer {
+import chapter_05.code_13.LaunderThrowable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Waiting for image download with Future.
+ */
+public abstract class FutureRenderer {
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    void renderPage(CharSequence source) {
+        final List<ImageInfo> imageInfos = scanForImageInfo(source);
+        Callable<List<ImageData>> task = () -> {
+            List<ImageData> result = new ArrayList<>();
+            for (ImageInfo imageInfo : imageInfos) {
+                result.add(imageInfo.downloadImage());
+            }
+            return result;
+        };
+        Future<List<ImageData>> future = executor.submit(task);
+        renderText(source);
+        try {
+            List<ImageData> imageData = future.get();
+            for (ImageData data : imageData) {
+                renderImage(data);
+            }
+        } catch (InterruptedException e) {
+            // Re-assert the thread's interrupted status
+            Thread.currentThread().interrupt();
+            // We don't need the result, so cancel the task too
+            future.cancel(true);
+        } catch (ExecutionException e) {
+            throw LaunderThrowable.launderThrowable(e.getCause());
+        }
+    }
+
+    abstract void renderText(CharSequence s);
+
+    abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+
+    abstract void renderImage(ImageData i);
+
+    interface ImageInfo {
+        ImageData downloadImage();
+    }
+
+    interface ImageData {
+    }
 }
