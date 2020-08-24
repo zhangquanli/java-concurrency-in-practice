47e538be24e21b18b11bcf0e5e2723356f356fd3
zhangquanli <502624709@qq.com>
2020-08-24 08:41:09 +0800

    6.3.5 CompletionService:Executor与BlockingQueue

---
+++ src/main/java/chapter_06/code_14/QueueingFuture.java
@@ -0,0 +1,10 @@
+package chapter_06.code_14;
+
+import java.util.concurrent.ExecutorCompletionService;
+
+/**
+ * QueueingFuture class used by ExecutorCompletionService. (See JDK source)
+ * {@link ExecutorCompletionService}
+ */
+public class QueueingFuture<V> {
+}
