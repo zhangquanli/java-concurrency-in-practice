2eefdac9d353b324be8f30bb3e4bf5c513bc6e38
zhangquanli <502624709@qq.com>
2020-08-27 08:39:46 +0800

    程序清单 7-4 Thread中的中断方法

---
+++ src/main/java/chapter_07/code_04/CodeThread.java
@@ -0,0 +1,9 @@
+package chapter_07.code_04;
+
+/**
+ * Interruption methods in Thread. (external link to Javadoc, shows all Thread methods, not just interruption-related)
+ * <p>
+ * http://java.sun.com/j2se/1.5.0/docs/api/java/lang/Thread.html#method_summary
+ */
+public class CodeThread {
+}
