6b4c28317c3e9109f009c4b4f5779841e36d77e5
zhangquanli <502624709@qq.com>
2020-09-14 08:58:08 +0800

    程序清单 7-20 使用私有的Executor，并且该Executor的生命周期受限于方法调用

---
+++ src/main/java/chapter_07/code_20/CheckForMail.java
@@ -0,0 +1,36 @@
+package chapter_07.code_20;
+
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Using a private Executor whose lifetime is bounded by a method call.
+ */
+public class CheckForMail {
+    public boolean checkMail(Set<String> hosts, long timeout, TimeUnit unit)
+            throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final AtomicBoolean hasNewMail = new AtomicBoolean(false);
+        try {
+            for (final String host : hosts) {
+                exec.execute(() -> {
+                    if (checkMail(host)) {
+                        hasNewMail.set(true);
+                    }
+                });
+            }
+        } finally {
+            exec.shutdown();
+            exec.awaitTermination(timeout, unit);
+        }
+        return hasNewMail.get();
+    }
+
+    private boolean checkMail(String host) {
+        // Check for mail
+        return false;
+    }
+}
