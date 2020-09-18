ec6a0d3532d56cf83468e74a908096860ebee127
zhangquanli <502624709@qq.com>
2020-09-18 08:40:24 +0800

    程序清单 8-1 在单线程Executor中任务发生死锁（不要这么做）

---
+++ src/main/java/chapter_08/code_01/ThreadDeadLock.java
@@ -0,0 +1,41 @@
+package chapter_08.code_01;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Task that deadlocks in a single-threaded Executor.
+ */
+public class ThreadDeadLock {
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public class LoadFileTask implements Callable<String> {
+        private final String fileName;
+
+        public LoadFileTask(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public String call() throws Exception {
+            // Here's where we would actually read the file
+            return "";
+        }
+    }
+
+    public class RenderPageTask implements Callable<String> {
+        public String call() throws Exception {
+            Future<String> header = exec.submit(new LoadFileTask("header.html"));
+            Future<String> footer = exec.submit(new LoadFileTask("footer.html"));
+            String page = renderBody();
+            // Will deadlock -- task waiting for result of subtask
+            return header.get() + page + footer.get();
+        }
+
+        private String renderBody() {
+            // Here's where we would actually render the page
+            return "";
+        }
+    }
+}
