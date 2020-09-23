5ecb9889829e21d09497bdc7a15084e61702a121
zhangquanli <502624709@qq.com>
2020-09-23 09:28:10 +0800

    程序清单 8-7 定制Thread基类

---
+++ src/main/java/chapter_08/code_07/MyAppThread.java
@@ -0,0 +1,59 @@
+package chapter_08.code_07;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Custom thread base class.
+ */
+public class MyAppThread extends Thread {
+    public static final String DEFAULT_NAME = "MyAppThread";
+    private static volatile boolean debugLifecycle = false;
+    private static final AtomicInteger created = new AtomicInteger();
+    private static final AtomicInteger alive = new AtomicInteger();
+    private static final Logger log = Logger.getAnonymousLogger();
+
+    public MyAppThread(Runnable runnable) {
+        this(runnable, DEFAULT_NAME);
+    }
+
+    public MyAppThread(Runnable runnable, String name) {
+        super(runnable, name + "-" + created.incrementAndGet());
+        setUncaughtExceptionHandler((t, e) ->
+                log.log(Level.SEVERE, "uncaught in thread " + t.getName(), e));
+    }
+
+    public void run() {
+        // Copy debug flag to ensure consistent value throughout.
+        boolean debug = debugLifecycle;
+        if (debug) {
+            log.log(Level.SEVERE, "Created " + getName());
+        }
+        try {
+            alive.incrementAndGet();
+            super.run();
+        } finally {
+            alive.decrementAndGet();
+            if (debug) {
+                log.log(Level.SEVERE, "Exiting " + getName());
+            }
+        }
+    }
+
+    public static int getThreadsCreated() {
+        return created.get();
+    }
+
+    public static int getThreadsAlive() {
+        return alive.get();
+    }
+
+    public static boolean getDebug() {
+        return debugLifecycle;
+    }
+
+    public static void setDebug(boolean b) {
+        debugLifecycle = b;
+    }
+}
