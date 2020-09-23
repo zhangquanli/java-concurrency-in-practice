538daf3fc96842720804a8e5f1f020130b91dfc9
zhangquanli <502624709@qq.com>
2020-09-23 09:09:04 +0800

    程序清单 8-4 使用Semaphore来控制任务的提交率

---
+++ src/main/java/chapter_08/code_04/BoundedExecutor.java
@@ -0,0 +1,36 @@
+package chapter_08.code_04;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Using a Semaphore to throttle task submission.
+ */
+@ThreadSafe
+public class BoundedExecutor {
+    private final Executor exec;
+    private final Semaphore semaphore;
+
+    public BoundedExecutor(Executor exec, int bound) {
+        this.exec = exec;
+        this.semaphore = new Semaphore(bound);
+    }
+
+    public void submitTask(final Runnable command) throws InterruptedException {
+        semaphore.acquire();
+        try {
+            exec.execute(() -> {
+                try {
+                    command.run();
+                } finally {
+                    semaphore.release();
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            semaphore.release();
+        }
+    }
+}
