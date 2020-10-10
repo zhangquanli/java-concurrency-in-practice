e0664e6b5b761650c75f456d72330988dee55718
zhangquanli <502624709@qq.com>
2020-10-10 09:04:48 +0800

    程序清单 8-14 用于谜题解决框架的链表节点

---
+++ src/main/java/chapter_08/code_14/PuzzleNode.java
@@ -0,0 +1,30 @@
+package chapter_08.code_14;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 程序清单 8-14 用于谜题解决框架的链表节点
+ *
+ * @param <P> 位置类
+ * @param <M> 移动类
+ */
+public class PuzzleNode<P, M> {
+    private final P pos;
+    private final M move;
+    private final PuzzleNode<P, M> prev;
+
+    public PuzzleNode(P pos, M move, PuzzleNode<P, M> prev) {
+        this.pos = pos;
+        this.move = move;
+        this.prev = prev;
+    }
+
+    List<M> asMoveList() {
+        List<M> solution = new LinkedList<>();
+        for (PuzzleNode<P, M> n = this; n.move != null; n = n.prev) {
+            solution.add(0, n.move);
+        }
+        return solution;
+    }
+}
