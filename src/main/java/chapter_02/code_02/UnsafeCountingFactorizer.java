a33d331ea6b9c0418e49f63ab3cef71491f27c8a
zhangquanli <502624709@qq.com>
2020-10-30 11:57:30 +0800

    程序清单 2-2 在没有同步的情况下统计已处理请求数量的 Servlet （不要这么做）

--- src/main/java/chapter_02/code_02/UnsafeCountingFactorizer.java
+++ src/main/java/chapter_02/code_02/UnsafeCountingFactorizer.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletResponse;
 import java.math.BigInteger;
 
 /**
- * Servlet that counts requests without the necessary synchronization.
+ * 程序清单 2-2 在没有同步的情况下统计已处理请求数量的 Servlet （不要这么做）
  */
 @NotThreadSafe
 public class UnsafeCountingFactorizer extends GenericServlet implements Servlet {
@@ -31,7 +31,6 @@ public class UnsafeCountingFactorizer extends GenericServlet implements Servlet 
     }
 
     private BigInteger[] factor(BigInteger i) {
-        // Doesn't really factor
         return new BigInteger[]{i};
     }
 
