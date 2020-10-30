dea6bb5ebabfd188b20f4729a47b878a3d737273
zhangquanli <502624709@qq.com>
2020-10-30 14:35:40 +0800

    程序清单 4-3 通过一个私有锁来保护状态

--- src/main/java/chapter_04/code_03/PrivateLock.java
+++ src/main/java/chapter_04/code_03/PrivateLock.java
@@ -1,14 +1,21 @@
 package chapter_04.code_03;
 
+import net.jcip.annotations.GuardedBy;
+
 /**
- * Guarding state with a private lock.
+ * 程序清单 4-3 通过一个私有锁来保护状态
  */
 public class PrivateLock {
     private final Object myLock = new Object();
+    @GuardedBy("myLock")
+    Widget widget;
 
     void someMethod() {
         synchronized (myLock) {
-
+            // 访问或修改 Widget 的状态
         }
+    }
+
+    private static class Widget {
     }
 }
