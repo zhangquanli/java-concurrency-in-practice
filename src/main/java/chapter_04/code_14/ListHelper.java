d250d1f382c8d228c812441ec89843915d9b1ff2
zhangquanli <502624709@qq.com>
2020-10-30 14:56:55 +0800

    程序清单 4-14 非线程安全的 “若没有则添加” （不要这么做）

--- src/main/java/chapter_04/code_14/ListHelper.java
+++ src/main/java/chapter_04/code_14/ListHelper.java
@@ -1,10 +1,16 @@
-package chapter_04;
+package chapter_04.code_14;
+
+import net.jcip.annotations.NotThreadSafe;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class BadListHelper<E> {
+/**
+ * 程序清单 4-14 非线程安全的 “若没有则添加” （不要这么做）
+ */
+@NotThreadSafe
+public class ListHelper<E> {
     public List<E> list = Collections.synchronizedList(new ArrayList<>());
 
     public synchronized boolean putIfAbsent(E x) {
