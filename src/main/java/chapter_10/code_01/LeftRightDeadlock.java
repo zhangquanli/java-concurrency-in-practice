3383cbea5647495289432280100e8842f630d91a
zhangquanli <502624709@qq.com>
2020-10-30 09:30:14 +0800

    程序清单 10-1 简单的锁顺序思索（不要这么做）

---
+++ src/main/java/chapter_10/code_01/LeftRightDeadlock.java
@@ -0,0 +1,31 @@
+package chapter_10.code_01;
+
+/**
+ * 程序清单 10-1 简单的锁顺序思索（不要这么做）
+ */
+public class LeftRightDeadlock {
+    private final Object left = new Object();
+    private final Object right = new Object();
+
+    public void leftRight() {
+        synchronized (left) {
+            synchronized (right) {
+                doSomething();
+            }
+        }
+    }
+
+    public void rightLeft() {
+        synchronized (right) {
+            synchronized (left) {
+                doSomethingElse();
+            }
+        }
+    }
+
+    private void doSomething() {
+    }
+
+    private void doSomethingElse() {
+    }
+}
