6bdc6ceec7c429e90b17cadd893f58c26bbff5cb
zhangquanli <502624709@qq.com>
2020-10-30 14:37:53 +0800

    程序清单 4-5 与 java.awt.Point 类似的可变 Point 类（不要这么做）

--- src/main/java/chapter_04/code_05/MutablePoint.java
+++ src/main/java/chapter_04/code_05/MutablePoint.java
@@ -3,7 +3,7 @@ package chapter_04.code_05;
 import net.jcip.annotations.NotThreadSafe;
 
 /**
- * Mutable Point class similar to java.awt.Point.
+ * 程序清单 4-5 与 java.awt.Point 类似的可变 Point 类（不要这么做）
  */
 @NotThreadSafe
 public class MutablePoint {
