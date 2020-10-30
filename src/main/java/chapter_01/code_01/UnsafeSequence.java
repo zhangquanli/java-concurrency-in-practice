52c2baf036c23eab181660f7f222501b8485e888
zhangquanli <502624709@qq.com>
2020-10-30 09:42:23 +0800

    程序清单 1-1 非线程安全的数值序列生成器

--- src/main/java/chapter_01/code_01/UnsafeSequence.java
+++ src/main/java/chapter_01/code_01/UnsafeSequence.java
@@ -3,14 +3,14 @@ package chapter_01.code_01;
 import net.jcip.annotations.NotThreadSafe;
 
 /**
- * Non-thread-safe sequence generator.
+ * 程序清单 1-1 非线程安全的数值序列生成器
  */
 @NotThreadSafe
 public class UnsafeSequence {
     private int value;
 
     /**
-     * Return a unique value.
+     * 返回一个唯一的值。
      */
     public int getNext() {
         return value++;
