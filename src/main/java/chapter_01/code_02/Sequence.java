8540b73017f43c066efef2fbf6a6905980249317
zhangquanli <502624709@qq.com>
2020-10-30 09:43:09 +0800

    程序清单 1-2 线程安全的数值序列生成器

--- src/main/java/chapter_01/code_02/Sequence.java
+++ src/main/java/chapter_01/code_02/Sequence.java
@@ -4,7 +4,7 @@ import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 /**
- * Thread-safe sequence generator.
+ * 程序清单 1-2 线程安全的数值序列生成器
  */
 @ThreadSafe
 public class Sequence {
