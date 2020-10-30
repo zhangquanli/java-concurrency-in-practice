2d3220a831c75b74243c2377a5376fb6d000e759
zhangquanli <502624709@qq.com>
2020-10-30 14:45:37 +0800

    程序清单 4-10 NumberRange 类不足以保护它的不变行条件（不要这么做）

--- src/main/java/chapter_04/code_10/NumberRange.java
+++ src/main/java/chapter_04/code_10/NumberRange.java
@@ -1,13 +1,17 @@
-package chapter_04;
+package chapter_04.code_10;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 程序清单 4-10 NumberRange 类不足以保护它的不变行条件（不要这么做）
+ */
 public class NumberRange {
-    // lower <= upper
+    // 不变性条件：lower <= upper
     private final AtomicInteger lower = new AtomicInteger(0);
     private final AtomicInteger upper = new AtomicInteger(0);
 
     public void setLower(int i) {
+        // 注意——不安全的“先检查后执行”
         if (i > upper.get()) {
             throw new IllegalArgumentException("can't set lower to " + i + " > upper");
         }
@@ -15,6 +19,7 @@ public class NumberRange {
     }
 
     public void setUpper(int i) {
+        // 注意——不安全的“先检查后执行”
         if (i < lower.get()) {
             throw new IllegalArgumentException("can't set upper to " + i + " < lower");
         }
