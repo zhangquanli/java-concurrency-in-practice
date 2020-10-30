839fdc931ba44b4bef9e1d2f25ca102e31881545
zhangquanli <502624709@qq.com>
2020-10-30 14:57:08 +0800

    程序清单 4-15 通过客户端加锁来实现 “若没有则添加”

--- src/main/java/chapter_04/code_15/ListHelper.java
+++ src/main/java/chapter_04/code_15/ListHelper.java
@@ -1,10 +1,16 @@
-package chapter_04;
+package chapter_04.code_15;
+
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class GoodListHelper<E> {
+/**
+ * 程序清单 4-15 通过客户端加锁来实现 “若没有则添加”
+ */
+@ThreadSafe
+public class ListHelper<E> {
     public List<E> list = Collections.synchronizedList(new ArrayList<>());
 
     public boolean putIfAbsent(E x) {
