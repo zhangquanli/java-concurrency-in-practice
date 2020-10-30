0096aca4d41435480a6e2bd9f768e77d9cfa5398
zhangquanli <502624709@qq.com>
2020-10-30 14:43:22 +0800

    程序清单 4-9 将线程安全性委托给多个状态变量

--- src/main/java/chapter_04/code_09/VisualComponent.java
+++ src/main/java/chapter_04/code_09/VisualComponent.java
@@ -1,10 +1,13 @@
-package chapter_04;
+package chapter_04.code_09;
 
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * 程序清单 4-9 将线程安全性委托给多个状态变量
+ */
 public class VisualComponent {
     private final List<KeyListener> keyListeners = new CopyOnWriteArrayList<>();
     private final List<MouseListener> mouseListeners = new CopyOnWriteArrayList<>();
