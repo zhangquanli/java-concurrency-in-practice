bc600016065d51460cd38292cf315f3fd0dc688f
zhangquanli <502624709@qq.com>
2020-10-30 11:58:39 +0800

    程序清单 2-3 延迟初始化中的竞态条件（不要这么做）

--- src/main/java/chapter_02/code_03/LazyInitRace.java
+++ src/main/java/chapter_02/code_03/LazyInitRace.java
@@ -3,7 +3,7 @@ package chapter_02.code_03;
 import net.jcip.annotations.NotThreadSafe;
 
 /**
- * Race condition in lazy initialization.
+ * 程序清单 2-3 延迟初始化中的竞态条件（不要这么做）
  */
 @NotThreadSafe
 public class LazyInitRace {
