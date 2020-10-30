fa736c0347ed095d77b1fcc780f7f5a26a3b6396
zhangquanli <502624709@qq.com>
2020-10-30 14:28:39 +0800

    程序清单 3-14 在没有足够同步的情况下发布对象（不要这么做）

--- src/main/java/chapter_03/code_14/StuffIntoPublic.java
+++ src/main/java/chapter_03/code_14/StuffIntoPublic.java
@@ -3,9 +3,10 @@ package chapter_03.code_14;
 import chapter_03.code_15.Holder;
 
 /**
- * Publishing an object without adequate synchronization.
+ * 程序清单 3-14 在没有足够同步的情况下发布对象（不要这么做）
  */
 public class StuffIntoPublic {
+    // 不安全的发布
     public Holder holder;
 
     public void initialize() {
