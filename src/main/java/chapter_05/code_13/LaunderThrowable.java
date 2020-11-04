77842804c1a66ed49622392c881aca7f03706bb8
zhangquanli <502624709@qq.com>
2020-11-04 09:09:15 +0800

    程序清单 5-13 强制将未检查的 Throwable 转换为 RuntimeException

--- src/main/java/chapter_05/code_13/LaunderThrowable.java
+++ src/main/java/chapter_05/code_13/LaunderThrowable.java
@@ -1,13 +1,11 @@
 package chapter_05.code_13;
 
 /**
- * Coercing an unchecked Throwable to a RuntimeException.
+ * 程序清单 5-13 强制将未检查的 Throwable 转换为 RuntimeException
  */
 public class LaunderThrowable {
-
     /**
-     * If the Throwable is an Error, throw it; if it is a
-     * RuntimeException return it, otherwise throw IllegalStateException.
+     * 如果 Throwable 是 Error，那么抛出它，如果是 RuntimeException，那么返回它，否则抛出 IllegalStateException
      */
     public static RuntimeException launderThrowable(Throwable t) {
         if (t instanceof RuntimeException) {
