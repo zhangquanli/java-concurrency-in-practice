fc2d03e8b30b1d82ea2324a758dc4fe4740c17ec
zhangquanli <502624709@qq.com>
2020-11-05 08:54:41 +0800

    程序清单 5-17 用 ConcurrentHashMap 替换 HashMap

--- src/main/java/chapter_05/code_17/Memoizer2.java
+++ src/main/java/chapter_05/code_17/Memoizer2.java
@@ -1,12 +1,11 @@
 package chapter_05.code_17;
 
-import chapter_05.code_16.Computable;
-
+import java.math.BigInteger;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Replacing HashMap with ConcurrentHashMap.
+ * 程序清单 5-17 用 ConcurrentHashMap 替换 HashMap
  */
 public class Memoizer2<A, V> implements Computable<A, V> {
     private final Map<A, V> cache = new ConcurrentHashMap<>();
@@ -24,4 +23,15 @@ public class Memoizer2<A, V> implements Computable<A, V> {
         }
         return result;
     }
+}
+
+class ExpensiveFunction implements Computable<String, BigInteger> {
+    public BigInteger compute(String arg) {
+        // 在经过长时间的计算后
+        return new BigInteger(arg);
+    }
+}
+
+interface Computable<A, V> {
+    V compute(A arg) throws InterruptedException;
 }
