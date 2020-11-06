e64cae59d7d8ff693f2f252bc414150cc2253040
zhangquanli <502624709@qq.com>
2020-11-06 09:02:09 +0800

    程序清单 7-8 在外部线程中安排中断（不要这么做）

--- src/main/java/chapter_07/code_08/TimedRun1.java
+++ src/main/java/chapter_07/code_08/TimedRun1.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Scheduling an interrupt on a borrowed thread.
+ * 程序清单 7-8 在外部线程中安排中断（不要这么做）
  */
 public class TimedRun1 {
     private static final ScheduledExecutorService cancelExec = Executors.newScheduledThreadPool(1);
