41ecf747e6f1f7eb234591af1ede15c0b30e1e6b
zhangquanli <502624709@qq.com>
2020-11-06 08:43:53 +0800

    程序清单 6-6 在调用线程中以同步方式执行所有任务的 Executor

--- src/main/java/chapter_06/code_06/WithinThreadExecutor.java
+++ src/main/java/chapter_06/code_06/WithinThreadExecutor.java
@@ -3,7 +3,7 @@ package chapter_06.code_06;
 import java.util.concurrent.Executor;
 
 /**
- * Executor that executes tasks synchronously in the calling thread.
+ * 程序清单 6-6 在调用线程中以同步方式执行所有任务的 Executor
  */
 public class WithinThreadExecutor implements Executor {
     public void execute(Runnable command) {
