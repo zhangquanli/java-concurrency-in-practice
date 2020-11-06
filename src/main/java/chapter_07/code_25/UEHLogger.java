b4ea2809878571f5d9de13503f787abd7df0ad99
zhangquanli <502624709@qq.com>
2020-11-06 09:42:25 +0800

    程序清单 7-25 将异常写入日志的 UncaughtExceptionHandler

--- src/main/java/chapter_07/code_25/UEHLogger.java
+++ src/main/java/chapter_07/code_25/UEHLogger.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
  * 程序清单 7-25 将异常写入日志的 UncaughtExceptionHandler
  */
 public class UEHLogger implements Thread.UncaughtExceptionHandler {
+
     public void uncaughtException(Thread t, Throwable e) {
         Logger logger = Logger.getAnonymousLogger();
         logger.log(Level.SEVERE, "Thread terminated with exception: " + t.getName(), e);
