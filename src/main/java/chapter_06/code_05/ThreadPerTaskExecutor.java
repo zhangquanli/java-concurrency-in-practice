d6793b0035ce3178174a0296f837008d2605044c
zhangquanli <502624709@qq.com>
2020-11-06 08:43:10 +0800

    程序清单 6-5 为每个请求启动一个新线程的 Executor

--- src/main/java/chapter_06/code_05/ThreadPerTaskExecutor.java
+++ src/main/java/chapter_06/code_05/ThreadPerTaskExecutor.java
@@ -3,7 +3,7 @@ package chapter_06.code_05;
 import java.util.concurrent.Executor;
 
 /**
- * Executor that starts a new thread for each task.
+ * 程序清单 6-5 为每个请求启动一个新线程的 Executor
  */
 public class ThreadPerTaskExecutor implements Executor {
     public void execute(Runnable command) {
